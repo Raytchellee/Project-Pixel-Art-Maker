e72c7a1e54289209f68fcc84926f33c666999b1c
Raytchellee <amakaoforkaja@gmail.com>
2022-05-15 10:36:05 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,44 @@
-// Select color input
-// Select size input
+const sizePicker = document.getElementById("sizePicker");
+const colorPicker = document.getElementById("colorPicker");
+const pixelCanvas = document.getElementById("pixelCanvas");
 
-// When size is submitted by the user, call makeGrid()
+const inputHeightElement = document.getElementById("inputHeight");
+const inputWidthElement = document.getElementById("inputWidth");
+
+sizePicker.addEventListener("submit", (event) => {
+  // prevents the page from refreshing when user clicks on the submit button
+  event.preventDefault();       
+
+  makeGrid();
+})
 
 function makeGrid() {
+  // get the number of rows and columns
+  const columns = inputWidthElement.value;
+  const rows = inputHeightElement.value;
 
-// Your code goes here!
+  // Clear out the former grid
+  pixelCanvas.innerHTML = "";
 
+  // display the new grid
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < columns; j++) {
+      const column = document.createElement("td");
+
+      // add event listeners to the grid cells to change the background color when a user clicks on it.
+      column.addEventListener("click", () => {
+        // get the color on the color picker
+        let color = colorPicker.value;
+
+        // set the background color of the cell
+        column.style.backgroundColor = color;
+      })
+
+      row.appendChild(column);
+    }
+
+    pixelCanvas.appendChild(row);
+  }
 }
